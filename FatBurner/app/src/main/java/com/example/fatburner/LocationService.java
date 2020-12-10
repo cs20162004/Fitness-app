d9ddeab91a35813ec0857861999763021447b47a
Alisheri Anvar <alisherianvar1998@gmail.com>
2020-12-10 17:22:15 +0900

    Forth commit

---
+++ FatBurner/app/src/main/java/com/example/fatburner/LocationService.java
@@ -0,0 +1,176 @@
+package com.example.fatburner;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DecimalFormat;
+
+public class LocationService extends Service {
+
+    public static double prevlat = 0;
+    public static double prevlong = 0;
+    public static double Km = 0;
+    public static double prevaltitude = 0;
+    public static double total_kcal = 0;
+    public static int counter = 0;
+    int k = 1;
+
+
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            if (locationResult != null && locationResult.getLastLocation() != null) {
+                double latitude = locationResult.getLastLocation().getLatitude();
+                double longitude = locationResult.getLastLocation().getLongitude();
+                double altitude = locationResult.getLastLocation().getAltitude();
+                Log.d("LOCATION_UPDATE", latitude + ", " + longitude);
+
+                if ((prevlat == 0 && prevlong == 0) || counter > 2){
+                    prevlat = latitude;
+                    prevlong = longitude;
+                    prevaltitude = altitude;
+                    counter = counter + 1;
+                }else if (MainActivity.running == true){
+                    Location locationprev = new Location("locationprev");
+                    locationprev.setLongitude(prevlong);
+                    locationprev.setLatitude(prevlat);
+
+                    Location currentlocation = new Location("currentlocation");
+                    currentlocation.setLatitude(latitude);
+                    currentlocation.setLongitude(longitude);
+
+                    double distance = locationprev.distanceTo(currentlocation) / 1000;
+                    Km = Km + distance;
+
+                    DecimalFormat df = new DecimalFormat("#.00");
+                    String km_id = df.format(Km);
+                    MainActivity.km_id.setText(km_id);
+
+                    String speed_id = df.format(Km * 3600 / (3 * k));
+                    if (distance * 1200 > 0.1){
+                        MainActivity.speed_id.setText(speed_id);
+                    }
+
+
+                    double inclination = (altitude - prevaltitude) / (distance * 10);
+                    String slope_id = df.format(inclination);
+                    //if (inclination != 0)
+                    MainActivity.slope_id.setText(slope_id);
+                    //else
+                    //    MainActivity.slope_id.setText("0.00");
+
+                    double kcal_burned = distance * 1.036 * MainActivity.user_weight;
+                    total_kcal = total_kcal + kcal_burned;
+
+                    if (altitude < prevaltitude){
+                        total_kcal = total_kcal - (0.066 * kcal_burned * inclination);
+                    }else if (altitude > prevaltitude){
+                        total_kcal = total_kcal + (0.12 * kcal_burned * inclination);
+                    }
+
+                    String kcal_id = df.format(total_kcal);
+                    MainActivity.kcal_id.setText(kcal_id);
+
+                    prevlat = latitude;
+                    prevlong = longitude;
+                    prevaltitude = altitude;
+                    k = k + 1;
+                }
+            }
+        }
+    };
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void startLocationService() {
+        String channelId = "location_notification_channel";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent resultIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channelId);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Location Service");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
+                NotificationChannel notificationChannel = new NotificationChannel(
+                        channelId,
+                        "Location Service",
+                        NotificationManager.IMPORTANCE_HIGH
+                );
+                notificationChannel.setDescription("This channel is used by location service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(4000);
+        locationRequest.setFastestInterval(2000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(this)
+                .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+
+        startForeground(Constants.LOCATION_SERVICE_ID, builder.build());
+
+    }
+
+    private void stopLocationService() {
+        LocationServices.getFusedLocationProviderClient(this)
+                .removeLocationUpdates(locationCallback);
+        stopForeground(true);
+        stopSelf();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null){
+            String action = intent.getAction();
+            if (action != null){
+                if (action.equals(Constants.ACTION_START_LOCATION_SERVICE)){
+                    startLocationService();
+                }else if (action.equals(Constants.ACTION_STOP_LOCATION_SERVICE)){
+                    stopLocationService();
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
---
+++ FatBurner/app/src/main/java/com/example/fatburner/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.fatburner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActivityChooserView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fatburner.LocationService;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+    public static boolean running;
+    private long pauseoffset;
+
+    private Chronometer chronometer;
+
+    public static TextView km_id;
+    public static TextView slope_id;
+    public static TextView kcal_id;
+    public static TextView speed_id;
+
+    public static EditText weight_id;
+    public static double user_weight = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        km_id = findViewById(R.id.km_id);
+        slope_id = findViewById(R.id.slope_id);
+        kcal_id = findViewById(R.id.kcal_id);
+        speed_id = findViewById(R.id.speed_id);
+        weight_id = findViewById(R.id.weight_id);
+
+
+
+        chronometer = findViewById(R.id.timer_id);
+        chronometer.setFormat("%s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+
+        findViewById(R.id.buttonstart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_weight = Double.valueOf(weight_id.getText().toString());
+
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_LOCATION_PERMISSION
+                    );
+                }else {
+                    startLocationService();
+                }
+
+                if (!running) {
+                    chronometer.setBase(SystemClock.elapsedRealtime() - pauseoffset);
+                    chronometer.start();
+                    running = true;
+                }
+            }
+        });
+
+        findViewById(R.id.buttonstop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationService();
+
+                if (running){
+                    chronometer.stop();
+                    pauseoffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    running = false;
+                }
+            }
+        });
+
+        findViewById(R.id.buttonreset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                pauseoffset = 0;
+
+                kcal_id.setText("0.0");
+                speed_id.setText("0.0");
+                slope_id.setText("0.00");
+                km_id.setText("0.00");
+
+                LocationService.Km = 0;
+                LocationService.total_kcal = 0;
+                LocationService.prevlat = 0;
+                LocationService.prevlong = 0;
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0){
+            if (grantResults[0] != PackageManager.PERMISSION_DENIED){
+                startLocationService();
+            }else{
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        if (activityManager != null){
+            for (ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)){
+                if (LocationService.class.getName().equals(service.service.getClassName())) {
+                    if (service.foreground){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private void startLocationService() {
+        if (!isLocationServiceRunning()){
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service started", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopLocationService() {
+        if (isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service stopped", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
